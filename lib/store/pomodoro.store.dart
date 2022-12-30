2a38b42ce718994194e8cd07e294e8c5762a2083
GabrielSchiavo <gabriel.kschiavo@gmail.com>
2022-12-30 19:08:36 -0300

    Alteração para evitar o uso de minutos negativos e maior que 99

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -17,13 +17,13 @@ abstract class _PomodoroStore with Store {
   bool iniciado = false;
 
   @observable
-  int minutos = 2;
+  int minutos = 1;
 
   @observable
   int segundos = 0;
 
   @observable
-  int tempoTrabalho = 2;
+  int tempoTrabalho = 1;
 
   @observable
   int tempoDescanso = 1;
@@ -36,7 +36,7 @@ abstract class _PomodoroStore with Store {
   @action
   void iniciar() {
     iniciado = true;
-    cronometro = Timer.periodic(const Duration(milliseconds: 50), (timer) {
+    cronometro = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (minutos == 0 && segundos == 0) {
         _trocarTipoIntervalo();
       } else if (segundos == 0) {
@@ -57,26 +57,48 @@ abstract class _PomodoroStore with Store {
   @action
   void reiniciar() {
     parar();
+    minutos = estaTrabalhando() ? tempoTrabalho : tempoDescanso;
+    segundos = 0;
   }
 
   @action
   void incrementarTempoTrabalho() {
-    tempoTrabalho++;
+    if (tempoTrabalho < 99) {
+      tempoTrabalho++;
+      if (estaTrabalhando()) {
+        reiniciar();
+      }
+    }
   }
 
   @action
   void decrementarTempoTrabalho() {
-    tempoTrabalho--;
+    if (tempoTrabalho > 1) {
+      tempoTrabalho--;
+      if (estaTrabalhando()) {
+        reiniciar();
+      }
+    }
   }
 
   @action
   void incrementarTempoDescanso() {
-    tempoDescanso++;
+    if (tempoDescanso < 99) {
+      tempoDescanso++;
+      if (estaDescansando()) {
+        reiniciar();
+      }
+    }
   }
 
   @action
   void decrementarTempoDescanso() {
-    tempoDescanso--;
+    if (tempoDescanso > 1) {
+      tempoDescanso--;
+      if (estaDescansando()) {
+        reiniciar();
+      }
+    }
   }
 
   bool estaTrabalhando() {
